0d67328079cfac15268e471b04a1839a012a3151
Sergio Santos <slns@portugalmail.pt>
2020-03-04 13:27:41 +0000

    Add user and index routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,49 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Surf Shop - Home' });
 });
 
+/* POST register page. */
+router.post('/', (req, res, next) => {
+  res.send('POST -> /register');
+});
+
+/* GET login page. */
+router.get('/login', (req, res, next) => {
+  res.send('GET -> /login');
+});
+
+/* POST login page. */
+router.post('/login', (req, res, next) => {
+  res.send('POST -> /login');
+});
+
+/* GET profile page. */
+router.get('/profile', (req, res, next) => {
+  res.send('GET -> /profile');
+});
+
+/* PUT profile/:user_id page. */
+router.put('/profile/:user_id', (req, res, next) => {
+  res.send('PUT -> /profile/:user_id');
+});
+
+/* GET forgot page. */
+router.get('/forgot', (req, res, next) => {
+  res.send('GET -> /forgot');
+});
+
+/* PUT forgot page. */
+router.put('/forgot', (req, res, next) => {
+  res.send('PUT -> /forgot');
+});
+
+/* GET reset/:token page. */
+router.get('/reset/:token', (req, res, next) => {
+  res.send('GET -> /reset/:token');
+});
+
+/* PUT reset/:token page. */
+router.put('/reset/:token', (req, res, next) => {
+  res.send('PUT -> /reset/:token');
+})
+
 module.exports = router;
